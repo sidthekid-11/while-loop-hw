2634b432889d263433874847ae645e67e44c4373
sidthekid-11 <rakheesiddhant2013@gmail.com>
2026-03-19 16:45:33 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 number = int(input("Enter a number: "))
 
-
-number = abs(number)
-
-count = 0
-
-
-if number == 0:
-    count = 1
-    
-else:
-    while number > 0:
-        number = number // 10   
-        count += 1            
-
-print("Total number of digits is:", count)
+while number > 0:
+    print(number)
+    number -= 1 
